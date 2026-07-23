9baf7eeaab56110d53bb46591b2148b2b7a0f011
Pete Williams <williape@gmail.com>
2026-07-23 23:34:25 +1000

    Initial Commit

---
+++ Transcriber/TranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriberApp.swift
+//  Transcriber
+//
+//  Created by pwilliams on 23/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct TranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranscriberTests/TranscriberTests.swift
@@ -0,0 +1,19 @@
+//
+//  TranscriberTests.swift
+//  TranscriberTests
+//
+//  Created by pwilliams on 23/7/2026.
+//
+
+import Testing
+@testable import Transcriber
+
+struct TranscriberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
